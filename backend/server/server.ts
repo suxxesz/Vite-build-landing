5281e8290e8656ded6c718ed5377813736956468
Zamesov Kirill <shrekopodobnii@gmail.com>
2026-08-24 22:20:38 +0300

    fix(server.ts): resolved problem with CORS access in origin URL

--- backend/server/server.ts
+++ backend/server/server.ts
@@ -39,7 +39,7 @@ async function start() {
 
   await app.register(cors, {
     origin: ['http://localhost:5173',
-      'https://suxxesz.github.io/core/',
+      'https://suxxesz.github.io',
     ]
 
   })
